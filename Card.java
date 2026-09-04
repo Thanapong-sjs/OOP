25f299d8792c79b1f8b05ca43e4dd01c9f91dfc6
Thanapong-sjs <tthanapong04@gmail.com>
2026-09-04 15:54:10 +0700

    Delete Card.java

--- Card.java
+++
@@ -1,9 +0,0 @@
-package lab7_2;
-
-public abstract class Card implements Withdrawable{
-	
-	public abstract String type();
-	public abstract double discount();
-	public abstract String getCardNumber();
-    
-}
